4e2829e2f54782f770b47a5a4461c9dbdd66e8b6
Shivam174268 <suryakumar9932@gmail.com>
2025-06-10 23:14:34 +0530

    Update List_Attendence.java

--- List_Attendence.java
+++ List_Attendence.java
@@ -1,61 +1,68 @@
 import java.awt.*;
 import javax.swing.*;
+import javax.swing.table.DefaultTableModel;
 import java.sql.*;
 import java.awt.event.*;
 
-public class List_Attendence extends JFrame implements ActionListener{
- 
+public class List_Attendence extends JFrame implements ActionListener {
+
     JTable j1;
     JButton b1;
-    String h[]={"Exm id","Date Time","First Half","Second Half"}; 
-    String d[][]=new String[15][4];
-    int i=0,j=0;
-    
-    List_Attendence()
-    {
-        super("View Employee Attendnce");
-        setSize(1000,400);
-        setLocation(450,200);
-        
-        try
-        {
-            Conn c= new Conn();
-            ResultSet rs=c.s.executeQuery("select * from attendence");
-            
-            while(rs.next())
-            {
-                d[i][j++]=rs.getString("id");
-                d[i][j++]=rs.getString("date_tm");
-                d[i][j++]=rs.getString("f_half");
-                d[i][j++]=rs.getString("s_half");
-                
-                i++;
-                j=0;
+    DefaultTableModel model;
+
+    List_Attendence() {
+        super("View Employee Attendance");
+        setSize(1000, 400);
+        setLocation(450, 200);
+
+        // Table headers
+        String[] columns = {"Emp id", "Date Time", "First Half", "Second Half"};
+        model = new DefaultTableModel(columns, 0);
+        j1 = new JTable(model);
+
+        try {
+            Conn c = new Conn();
+            ResultSet rs = c.s.executeQuery("SELECT * FROM attendence");
+
+            // Dynamically add rows
+            while (rs.next()) {
+                String[] row = {
+                    rs.getString("id"),
+                    rs.getString("date_tm"),
+                    rs.getString("f_half"),
+                    rs.getString("s_half")
+                };
+                model.addRow(row);
             }
-            j1=new JTable(d,h);
-        }
-        catch(Exception e)
-        {
+        } catch (Exception e) {
             e.printStackTrace();
         }
-        b1=new JButton("Print");
-        add(b1,"South");
-        JScrollPane s1= new JScrollPane(j1);
+
+        // Add table inside scroll pane
+        JScrollPane s1 = new JScrollPane(j1);
         add(s1);
+
+        b1 = new JButton("Print");
+        add(b1, BorderLayout.SOUTH);
+
         b1.addActionListener(this);
+
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         setVisible(true);
     }
-    
-    public void actionPerformed(ActionEvent ae)
-    {
-        try{
+
+    public void actionPerformed(ActionEvent ae) {
+        try {
             j1.print();
-        }catch(Exception e){}
-    }
-    public static void main(String[] args) {
-     
-        new List_Attendence();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 
-            
+    public static void main(String[] args) {
+        new List_Attendence();
+    }
 }
+
+        
+      
