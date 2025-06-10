207266bac39859924c5bf37e7a2d6f635d2947cb
Shivam174268 <suryakumar9932@gmail.com>
2025-06-10 23:03:48 +0530

    Update Conn.java

--- Conn.java
+++ Conn.java
@@ -1,21 +1,28 @@
 import java.sql.*;
 import java.util.logging.Level;
 import java.util.logging.Logger;
-public class Conn 
-{
+
+public class Conn {
     public Connection c;
     public Statement s;
-            
-    public Conn()
-    {
-        try
-        {
-            Class.forName("com.mysql.jdbc.Driver");
-            c=DriverManager.getConnection("jdbc:mysql:///payroll","root","");
-            s=c.createStatement();
-            
+
+    public Conn() {
+        try {
+            // Correct MySQL JDBC Driver
+            Class.forName("com.mysql.cj.jdbc.Driver");
+
+            // Correct MySQL connection string (update port, db name if needed)
+            c = DriverManager.getConnection(
+                    "jdbc:mysql://localhost:3306/payroll?useSSL=false&serverTimezone=UTC",
+                    "root",
+                    "");
+
+            // Create statement object
+            s = c.createStatement();
+
         } catch (Exception e) {
-            e.printStackTrace();
+            // Proper logging
+            Logger.getLogger(Conn.class.getName()).log(Level.SEVERE, null, e);
         }
     }
 }
